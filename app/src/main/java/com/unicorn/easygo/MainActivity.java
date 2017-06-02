68f62a88893bde337f4e6ea081d32a2fb12d5c78
haocdp <haoc_dp@163.com>
2017-06-03 01:22:27 +0800

    首页整体页面

--- app/src/main/java/com/unicorn/easygo/MainActivity.java
+++ app/src/main/java/com/unicorn/easygo/MainActivity.java
@@ -1,13 +1,60 @@
 package com.unicorn.easygo;
 
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private View preOrderView, scanRecordView;
+    private ViewPager viewPager;
+    private List<View> viewList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        LayoutInflater inflater = getLayoutInflater();
+        preOrderView = inflater.inflate(R.layout.pre_order_list_layout, null);
+        scanRecordView = inflater.inflate(R.layout.scan_record_layout, null);
+
+        viewList = new ArrayList<>();
+        viewList.add(preOrderView);
+        viewList.add(scanRecordView);
+
+        PagerAdapter pagerAdapter = new PagerAdapter() {
+            @Override
+            public int getCount() {
+                return viewList.size();
+            }
+
+            @Override
+            public boolean isViewFromObject(View view, Object object) {
+                return view == object;
+            }
+
+            @Override
+            public void destroyItem(ViewGroup container, int position, Object object) {
+                super.destroyItem(container, position, object);
+                container.removeView(viewList.get(position));
+            }
+
+            @Override
+            public Object instantiateItem(ViewGroup container, int position) {
+                container.addView(viewList.get(position));
+                return viewList.get(position);
+            }
+        };
+
+        viewPager.setAdapter(pagerAdapter);
     }
 }
