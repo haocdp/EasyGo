75bde18fafe33ad6b7ef32d8fc933a1d829f17d6
wallencefans <363868404@qq.com>
2017-06-05 12:31:17 +0800

    frament修改

--- app/src/main/java/com/unicorn/easygo/MyPagerAdapter.java
+++ app/src/main/java/com/unicorn/easygo/MyPagerAdapter.java
@@ -5,26 +5,22 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import java.util.List;
 
-
 public class MyPagerAdapter extends FragmentPagerAdapter {
 
     private List<Fragment> mFragments;
 
     public MyPagerAdapter(FragmentManager fm,List<Fragment> fragments) {
         super(fm);
-        // TODO Auto-generated constructor stub
         mFragments=fragments;
     }
 
     @Override
     public Fragment getItem(int arg0) {
-        // TODO Auto-generated method stub
         return mFragments.get(arg0);
     }
 
     @Override
     public int getCount() {
-        // TODO Auto-generated method stub
         return mFragments.size();
     }
 
