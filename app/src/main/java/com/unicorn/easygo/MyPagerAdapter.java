f5c5686099d88431f59765f7b8829a1afec9d6ce
wallencefans <363868404@qq.com>
2017-06-05 12:26:09 +0800

    frament修改

--- app/src/main/java/com/unicorn/easygo/MyPagerAdapter.java
+++ app/src/main/java/com/unicorn/easygo/MyPagerAdapter.java
@@ -3,16 +3,8 @@ package com.unicorn.easygo;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.PagerAdapter;
-import android.view.View;
-import android.view.ViewGroup;
-
-import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Created by Jay on 2015/10/8 0008.
- */
 
 public class MyPagerAdapter extends FragmentPagerAdapter {
 
@@ -37,35 +29,3 @@ public class MyPagerAdapter extends FragmentPagerAdapter {
     }
 
 }
-//public class MyPagerAdapter extends PagerAdapter {
-//    private ArrayList<View> viewLists;
-//
-//    public MyPagerAdapter() {
-//    }
-//
-//    public MyPagerAdapter(ArrayList<View> viewLists) {
-//        super();
-//        this.viewLists = viewLists;
-//    }
-//
-//    @Override
-//    public int getCount() {
-//        return viewLists.size();
-//    }
-//
-//    @Override
-//    public boolean isViewFromObject(View view, Object object) {
-//        return view == object;
-//    }
-//
-//    @Override
-//    public Object instantiateItem(ViewGroup container, int position) {
-//        container.addView(viewLists.get(position));
-//        return viewLists.get(position);
-//    }
-//
-//    @Override
-//    public void destroyItem(ViewGroup container, int position, Object object) {
-//        container.removeView(viewLists.get(position));
-//    }
-//}
