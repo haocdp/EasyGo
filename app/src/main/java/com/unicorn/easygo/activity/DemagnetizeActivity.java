a8a0a3e48000b64dad8e948b0ea467f892482c16
haocdp <haoc_dp@163.com>
2017-06-22 21:27:46 +0800

    支付界面编写

---
+++ app/src/main/java/com/unicorn/easygo/activity/DemagnetizeActivity.java
@@ -0,0 +1,15 @@
+package com.unicorn.easygo.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.unicorn.easygo.R;
+
+public class DemagnetizeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_demagnetize);
+    }
+}
