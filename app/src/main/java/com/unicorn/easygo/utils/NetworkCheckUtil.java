f0bd24846cb9627502951e2664b4911f55ba4e04
haocdp <haoc_dp@163.com>
2017-06-24 23:13:13 +0800

    增加网络检查工具类

---
+++ app/src/main/java/com/unicorn/easygo/utils/NetworkCheckUtil.java
@@ -0,0 +1,35 @@
+package com.unicorn.easygo.utils;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+/**
+ * Created by haoc_dp on 2017/6/24.
+ */
+
+public class NetworkCheckUtil {
+
+    /**
+     * 检测当的网络（WLAN、3G/2G）状态
+     * @param context Context
+     * @return true 表示网络可用
+     */
+    public static boolean isNetworkAvailable(Context context) {
+        ConnectivityManager connectivity = (ConnectivityManager) context
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (connectivity != null) {
+            NetworkInfo info = connectivity.getActiveNetworkInfo();
+            if (info != null && info.isConnected())
+            {
+                // 当前网络是连接的
+                if (info.getState() == NetworkInfo.State.CONNECTED)
+                {
+                    // 当前所连接的网络可用
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
