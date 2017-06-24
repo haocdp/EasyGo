348a99f5efd96ae5bf3aa2374332181eebfe34e3
haocdp <haoc_dp@163.com>
2017-06-24 23:11:26 +0800

    地图自动定位

--- app/src/main/java/com/unicorn/easygo/activity/MarketRecommendActivity.java
+++ app/src/main/java/com/unicorn/easygo/activity/MarketRecommendActivity.java
@@ -1,27 +1,94 @@
 package com.unicorn.easygo.activity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.location.Location;
+import android.location.LocationListener;
 import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.Window;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
+import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
+import com.amap.api.location.AMapLocationListener;
 import com.amap.api.maps.AMap;
 import com.amap.api.maps.CameraUpdateFactory;
+import com.amap.api.maps.LocationSource;
 import com.amap.api.maps.MapView;
 import com.amap.api.maps.UiSettings;
+import com.amap.api.maps.model.BitmapDescriptor;
+import com.amap.api.maps.model.BitmapDescriptorFactory;
+import com.amap.api.maps.model.CircleOptions;
+import com.amap.api.maps.model.LatLng;
+import com.amap.api.maps.model.LatLngBounds;
+import com.amap.api.maps.model.Marker;
+import com.amap.api.maps.model.MarkerOptions;
 import com.amap.api.maps.model.MyLocationStyle;
+import com.amap.api.maps.model.MyTrafficStyle;
+import com.amap.api.services.core.AMapException;
+import com.amap.api.services.core.LatLonPoint;
+import com.amap.api.services.core.PoiItem;
+import com.amap.api.services.core.SuggestionCity;
+import com.amap.api.services.poisearch.PoiResult;
+import com.amap.api.services.poisearch.PoiSearch;
 import com.unicorn.easygo.R;
+import com.unicorn.easygo.utils.NetworkCheckUtil;
+import com.unicorn.easygo.utils.ToastUtil;
 
-public class MarketRecommendActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
+public class MarketRecommendActivity extends AppCompatActivity implements
+        PoiSearch.OnPoiSearchListener, AMap.OnMyLocationChangeListener{
+
+    private AMap aMap;
     private MapView mMapView = null;
     private UiSettings mUiSettings = null;
+
+    /**
+     * poi搜索参数
+     */
+    private PoiSearch.Query query;
+    private PoiSearch poiSearch;
+    private LatLonPoint latLonPoint = new LatLonPoint(39.993743, 116.472995);
+    private PoiResult poiResult;
+    private List<PoiItem> poiItems;
+    private Marker mlastMarker;
+    private myPoiOverlay poiOverlay;// poi图层
+
+    private int[] markers = {R.drawable.poi_marker_1,
+            R.drawable.poi_marker_2,
+            R.drawable.poi_marker_3,
+            R.drawable.poi_marker_4,
+            R.drawable.poi_marker_5,
+            R.drawable.poi_marker_6,
+            R.drawable.poi_marker_7,
+            R.drawable.poi_marker_8,
+            R.drawable.poi_marker_9,
+            R.drawable.poi_marker_10,
+            R.drawable.poi_marker_1,
+            R.drawable.poi_marker_2,
+            R.drawable.poi_marker_3,
+            R.drawable.poi_marker_4,
+            R.drawable.poi_marker_5,
+            R.drawable.poi_marker_6,
+            R.drawable.poi_marker_7,
+            R.drawable.poi_marker_8,
+            R.drawable.poi_marker_9,
+            R.drawable.poi_marker_10
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,23 +99,58 @@ public class MarketRecommendActivity extends AppCompatActivity {
         //在activity执行onCreate时执行mMapView.onCreate(savedInstanceState)，创建地图
         mMapView.onCreate(savedInstanceState);
 
-        AMap aMap = null;
+        if (checkNetwork()) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                    == PackageManager.PERMISSION_GRANTED) {
+                initMap();
+                setViews();
+            } else {
+                boolean shouldShow = ActivityCompat.shouldShowRequestPermissionRationale(
+                        this, Manifest.permission.ACCESS_COARSE_LOCATION
+                );
+
+            }
+        }
+        //doSearchQuery();
+    }
+
+    private void initMap() {
         if (aMap == null) {
             aMap = mMapView.getMap();
         }
-        aMap.moveCamera(CameraUpdateFactory.zoomTo(16f));
+
         mUiSettings = aMap.getUiSettings();
         //aMap.setMapType(AMap.MAP_TYPE_SATELLITE);
+        //mUiSettings.setZoomControlsEnabled(false);
         mUiSettings.setScaleControlsEnabled(true);
         //mUiSettings.setMyLocationButtonEnabled(true);//设置默认定位按钮是否显示，非必需设置。
 
-        aMap.setMyLocationEnabled(true);// 设置为true表示启动显示定位蓝点，false表示隐藏定位蓝点并不进行定位，默认是false。
         MyLocationStyle myLocationStyle = new MyLocationStyle();//初始化定位蓝点样式类
-        myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE);//连续定位、且将视角移动到地图中心点，定位点依照设备方向旋转，并且会跟随设备移动。（1秒1次定位）如果不设置myLocationType，默认也会执行此种模式。
-        myLocationStyle.interval(2000); //设置连续定位模式下的定位间隔，只在连续定位模式下生效，单次定位模式下不会生效。单位为毫秒。
-        aMap.setMyLocationStyle(myLocationStyle);//设置定位蓝点的Style
+        myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATE);//连续定位、且将视角移动到地图中心点，定位点依照设备方向旋转，并且会跟随设备移动。（1秒1次定位）如果不设置myLocationType，默认也会执行此种模式。
+        //myLocationStyle.interval(2000); //设置连续定位模式下的定位间隔，只在连续定位模式下生效，单次定位模式下不会生效。单位为毫秒。
+        aMap.setMyLocationStyle(myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATE));//设置定位蓝点的Style
 
-        setViews();
+        aMap.setMyLocationEnabled(true);// 设置为true表示启动显示定位蓝点，false表示隐藏定位蓝点并不进行定位，默认是false。
+        aMap.setOnMyLocationChangeListener(this);
+
+
+        //自定义实时交通信息的颜色样式
+        MyTrafficStyle myTrafficStyle = new MyTrafficStyle();
+        myTrafficStyle.setSeriousCongestedColor(0xff92000a);
+        myTrafficStyle.setCongestedColor(0xffea0312);
+        myTrafficStyle.setSlowColor(0xffff7508);
+        myTrafficStyle.setSmoothColor(0xff00a209);
+        aMap.setMyTrafficStyle(myTrafficStyle);
+        aMap.setTrafficEnabled(true);
+        aMap.moveCamera(CameraUpdateFactory.zoomTo(14f));
+    }
+
+    private boolean checkNetwork() {
+        if (!NetworkCheckUtil.isNetworkAvailable(this)) {
+            ToastUtil.show(this, "当前没有网络连接");
+            return false;
+        }
+        return true;
     }
 
     private void setViews(){
@@ -251,4 +353,241 @@ public class MarketRecommendActivity extends AppCompatActivity {
         mMapView.onSaveInstanceState(outState);
     }
 
+    private void doSearchQuery(Location location) {
+        query = new PoiSearch.Query("", "060100|060400", "027");
+        query.setPageSize(20);
+        query.setPageNum(1);
+        poiSearch = new PoiSearch(this, query);
+        poiSearch.setOnPoiSearchListener(this);
+        poiSearch.setBound(new PoiSearch.SearchBound(
+                new LatLonPoint(location.getLongitude(),location.getLatitude()),
+                2000));
+        poiSearch.searchPOIAsyn();
+    }
+
+    @Override
+    public void onPoiSearched(PoiResult result, int rcode) {
+        //Log.i("poiResult", poiResult.toString());
+        if (rcode == AMapException.CODE_AMAP_SUCCESS) {
+            if (result != null && result.getQuery() != null) {// 搜索poi的结果
+                if (result.getQuery().equals(query)) {// 是否是同一条
+                    poiResult = result;
+                    poiItems = poiResult.getPois();// 取得第一页的poiitem数据，页数从数字0开始
+                    List<SuggestionCity> suggestionCities = poiResult
+                            .getSearchSuggestionCitys();// 当搜索不到poiitem数据时，会返回含有搜索关键字的城市信息
+                    if (poiItems != null && poiItems.size() > 0) {
+                        //并还原点击marker样式
+                        if (mlastMarker != null) {
+                            //resetlastmarker();
+                        }
+                        //清理之前搜索结果的marker
+                        if (poiOverlay !=null) {
+                            //poiOverlay.removeFromMap();
+                        }
+                        //aMap.clear();
+                        poiOverlay = new myPoiOverlay(aMap, poiItems);
+                        poiOverlay.addToMap();
+                        poiOverlay.zoomToSpan();
+
+                    } else if (suggestionCities != null
+                            && suggestionCities.size() > 0) {
+                        showSuggestCity(suggestionCities);
+                    } else {
+                        ToastUtil.show(MarketRecommendActivity.this,
+                                R.string.no_result);
+                    }
+                }
+            } else {
+                ToastUtil.show(MarketRecommendActivity.this, R.string.no_result);
+            }
+        } else  {
+            ToastUtil.showerror(this.getApplicationContext(), rcode);
+        }
+    }
+
+    @Override
+    public void onPoiItemSearched(PoiItem poiItem, int i) {
+
+    }
+
+    @Override
+    public void onMyLocationChange(Location location) {
+        doSearchQuery(location);
+
+        // 定位回调监听
+        if(location != null) {
+            Log.e("amap", "onMyLocationChange 定位成功， lat: " + location.getLatitude() + " lon: " + location.getLongitude());
+            Bundle bundle = location.getExtras();
+            if(bundle != null) {
+                int errorCode = bundle.getInt(MyLocationStyle.ERROR_CODE);
+                String errorInfo = bundle.getString(MyLocationStyle.ERROR_INFO);
+                // 定位类型，可能为GPS WIFI等，具体可以参考官网的定位SDK介绍
+                int locationType = bundle.getInt(MyLocationStyle.LOCATION_TYPE);
+
+                /*
+                errorCode
+                errorInfo
+                locationType
+                */
+                Log.e("amap", "定位信息， code: " + errorCode + " errorInfo: " + errorInfo + " locationType: " + locationType );
+            } else {
+                Log.e("amap", "定位信息， bundle is null ");
+
+            }
+
+        } else {
+            Log.e("amap", "定位失败");
+        }
+    }
+
+    // 将之前被点击的marker置为原来的状态
+    private void resetlastmarker() {
+        int index = poiOverlay.getPoiIndex(mlastMarker);
+        if (index < 10) {
+            mlastMarker.setIcon(BitmapDescriptorFactory
+                    .fromBitmap(BitmapFactory.decodeResource(
+                            getResources(),
+                            markers[index])));
+        }else {
+            mlastMarker.setIcon(BitmapDescriptorFactory.fromBitmap(
+                    BitmapFactory.decodeResource(getResources(), R.drawable.marker_other_highlight)));
+        }
+        mlastMarker = null;
+
+    }
+
+    /**
+     * poi没有搜索到数据，返回一些推荐城市的信息
+     */
+    private void showSuggestCity(List<SuggestionCity> cities) {
+        String infomation = "推荐城市\n";
+        for (int i = 0; i < cities.size(); i++) {
+            infomation += "城市名称:" + cities.get(i).getCityName() + "城市区号:"
+                    + cities.get(i).getCityCode() + "城市编码:"
+                    + cities.get(i).getAdCode() + "\n";
+        }
+        ToastUtil.show(this, infomation);
+
+    }
+
+
+    /**
+     * 自定义PoiOverlay
+     *
+     */
+
+    private class myPoiOverlay {
+        private AMap mamap;
+        private List<PoiItem> mPois;
+        private ArrayList<Marker> mPoiMarks = new ArrayList<Marker>();
+        public myPoiOverlay(AMap amap ,List<PoiItem> pois) {
+            mamap = amap;
+            mPois = pois;
+        }
+
+        /**
+         * 添加Marker到地图中。
+         * @since V2.1.0
+         */
+        public void addToMap() {
+            for (int i = 0; i < mPois.size(); i++) {
+                Marker marker = mamap.addMarker(getMarkerOptions(i));
+                PoiItem item = mPois.get(i);
+                marker.setObject(item);
+                mPoiMarks.add(marker);
+            }
+        }
+
+        /**
+         * 去掉PoiOverlay上所有的Marker。
+         *
+         * @since V2.1.0
+         */
+        public void removeFromMap() {
+            for (Marker mark : mPoiMarks) {
+                mark.remove();
+            }
+        }
+
+        /**
+         * 移动镜头到当前的视角。
+         * @since V2.1.0
+         */
+        public void zoomToSpan() {
+            if (mPois != null && mPois.size() > 0) {
+                if (mamap == null)
+                    return;
+                LatLngBounds bounds = getLatLngBounds();
+                mamap.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds, 100));
+            }
+        }
+
+        private LatLngBounds getLatLngBounds() {
+            LatLngBounds.Builder b = LatLngBounds.builder();
+            for (int i = 0; i < mPois.size(); i++) {
+                b.include(new LatLng(mPois.get(i).getLatLonPoint().getLatitude(),
+                        mPois.get(i).getLatLonPoint().getLongitude()));
+            }
+            return b.build();
+        }
+
+        private MarkerOptions getMarkerOptions(int index) {
+            return new MarkerOptions()
+                    .position(
+                            new LatLng(mPois.get(index).getLatLonPoint()
+                                    .getLatitude(), mPois.get(index)
+                                    .getLatLonPoint().getLongitude()))
+                    .title(getTitle(index)).snippet(getSnippet(index))
+                    .icon(getBitmapDescriptor(index));
+        }
+
+        protected String getTitle(int index) {
+            return mPois.get(index).getTitle();
+        }
+
+        protected String getSnippet(int index) {
+            return mPois.get(index).getSnippet();
+        }
+
+        /**
+         * 从marker中得到poi在list的位置。
+         *
+         * @param marker 一个标记的对象。
+         * @return 返回该marker对应的poi在list的位置。
+         * @since V2.1.0
+         */
+        public int getPoiIndex(Marker marker) {
+            for (int i = 0; i < mPoiMarks.size(); i++) {
+                if (mPoiMarks.get(i).equals(marker)) {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        /**
+         * 返回第index的poi的信息。
+         * @param index 第几个poi。
+         * @return poi的信息。poi对象详见搜索服务模块的基础核心包（com.amap.api.services.core）中的类 <strong><a href="../../../../../../Search/com/amap/api/services/core/PoiItem.html" title="com.amap.api.services.core中的类">PoiItem</a></strong>。
+         * @since V2.1.0
+         */
+        public PoiItem getPoiItem(int index) {
+            if (index < 0 || index >= mPois.size()) {
+                return null;
+            }
+            return mPois.get(index);
+        }
+
+        protected BitmapDescriptor getBitmapDescriptor(int arg0) {
+            if (arg0 < 10) {
+                BitmapDescriptor icon = BitmapDescriptorFactory.fromBitmap(
+                        BitmapFactory.decodeResource(getResources(), markers[arg0]));
+                return icon;
+            }else {
+                BitmapDescriptor icon = BitmapDescriptorFactory.fromBitmap(
+                        BitmapFactory.decodeResource(getResources(), R.drawable.marker_other_highlight));
+                return icon;
+            }
+        }
+    }
 }
