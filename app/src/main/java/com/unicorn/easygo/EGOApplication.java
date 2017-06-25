1abd5dbe6219ae2adcf7d3357cdb8df9319ddc52
haocdp <haoc_dp@163.com>
2017-06-25 17:05:43 +0800

    增加EGOApplication类

---
+++ app/src/main/java/com/unicorn/easygo/EGOApplication.java
@@ -0,0 +1,31 @@
+package com.unicorn.easygo;
+
+import android.app.Application;
+
+import com.unicorn.easygo.entity.UserProfile;
+
+/**
+ * Created by haoc_dp on 2017/6/25.
+ */
+
+public class EGOApplication extends Application {
+
+    private static EGOApplication egoApplication;
+
+    private static EGOApplication getInstance() {
+        return egoApplication;
+    }
+
+    private UserProfile userProfile;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        egoApplication = this;
+        userProfile = new UserProfile();
+    }
+
+    private UserProfile getUserProfile() {
+        return userProfile;
+    }
+}
---
+++ app/src/main/java/com/unicorn/easygo/entity/UserProfile.java
@@ -0,0 +1,36 @@
+package com.unicorn.easygo.entity;
+
+/**
+ * Created by haoc_dp on 2017/6/25.
+ */
+
+public class UserProfile {
+
+    private String username;
+    private String sex;
+    private String imageUrl;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
